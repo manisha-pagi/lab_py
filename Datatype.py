8910959bfc6b21c95777501a34217850eb6fc30b
manisha-pagi <thakorvimal101@gmail.com>
2026-01-06 09:21:48 +0530

    Add files via upload

---
+++ Datatype.py
@@ -0,0 +1,46 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x="hello"
+print(x);   #str
+hello
+y=20
+print(y);  #int
+20
+y=20.5
+print(y);  #float
+20.5
+a=1j
+print(a);  #complex
+1j
+b=["apple","banana","cherry"]
+print(b);   #list
+['apple', 'banana', 'cherry']
+b=["red","blue","green"]
+print(b);   #tuple
+['red', 'blue', 'green']
+x=range(->6)
+SyntaxError: invalid syntax
+x=range(6)
+print(x);  #range
+range(0, 6)
+x={"name":"john","age":"18"}
+print(x);   #dict
+{'name': 'john', 'age': '18'}
+s={"a","b","c"}
+print(s);  #set
+{'c', 'b', 'a'}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
