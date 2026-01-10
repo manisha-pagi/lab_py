5e6879ed467e87e3c54c723035fe41f9f0280d28
manisha-pagi <thakorvimal101@gmail.com>
2026-01-10 09:02:05 +0530

    Assigment=1

---
+++ assigment1.py
@@ -0,0 +1,15 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("Hello, World!")
+
+Hello, World!
+print("Welcome to Python programming")
+
+Welcome to Python programming
+name="manisha"
+age="18"
+print("name:",name,"age:",age)
+name: manisha age: 18
+name:"vimal"
+print(f"hello;{name}!welcome to python")
+hello;manisha!welcome to python
