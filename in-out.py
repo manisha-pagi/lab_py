bf342ab9c7234dcfc73ad36bec845b283d02fe7e
manisha-pagi <thakorvimal101@gmail.com>
2026-01-31 08:10:05 +0530

    Input_Output

---
+++ in-out.py
@@ -0,0 +1,13 @@
+#output
+print("good morning!")
+#output
+name=input("enter your name: ")
+print("good morning ",name)
+#example
+name=input("Enter your name:")
+rn=input("Enter your roll no:")
+en=input("Enrolment no:")
+dob=input("Enter yuor date of birth:")
+
+
+
