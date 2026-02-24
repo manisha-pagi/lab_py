2d464a8aed49f8f091ed3fcd7b6f5e04bcd7d1b4
manisha-pagi <thakorvimal101@gmail.com>
2026-02-24 08:10:57 +0530

    While loop task

---
+++ whileloop.py
@@ -0,0 +1,19 @@
+#1.print number from 1 to 10.
+i=1
+while i<=10:
+    print(i)
+    i=i+1
+#2.sum of first n natural numbers.
+n=int (input("Enter n:"))
+i=1
+s=0
+while i<=n:
+    s=s+i
+    i=i+1
+    print("sum=",s) 
+#3 table of a number
+num = int(input("enter your number:"))
+i = 1
+while i<= 10:
+    print(num,"x",i,"=",num*i)
+    i=i+1
