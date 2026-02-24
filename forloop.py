9fdff0d9e89aa649c65c7f2e7f348b8845e18a87
manisha-pagi <thakorvimal101@gmail.com>
2026-02-24 08:18:10 +0530

    For loop task

---
+++ forloop.py
@@ -0,0 +1,34 @@
+#1
+for i in range(1,6):
+    print(i)
+#2
+for i in range(3):
+    print("good morning") 
+#task 1
+for i in range(1,11):
+    print(i)
+#2
+for i in range(1,21):
+    if i%2==0:
+        print(i)
+#3
+for i in range(1,16):
+    if i%2!=0:
+        print(i)                      
+#4
+for i in range(1,11):
+    print("5x",i,"=",5*i)
+#5
+name="Atmiya"
+for letter in name:
+    print(letter)
+#6
+total=0
+for i in range(1,6):
+    total=total+i
+    print("sum is:",total)
+#7
+numbers=[10,20,30,40]  
+for n in numbers:
+    print(n)  
+                    
